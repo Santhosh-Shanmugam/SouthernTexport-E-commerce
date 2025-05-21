ead19405f91ca899ca5160e5c0ca7f3e38dd930d
Navin-VijayaKumar <navinv10122004@gmail.com>
2025-05-21 13:36:49 +0530

    Filter update

--- Frontend/src/Pages/Kids/Kids.jsx
+++ Frontend/src/Pages/Kids/Kids.jsx
@@ -12,28 +12,28 @@ const Kids = () => {
   return (
     <div className="item-banner-con">
 
-    <div className="banner">
+      <div className="banner">
         <img src={stpKids}></img>
-    </div>
-    <div className="items-con">
-        {all_product.filter((item) => item.category === "Men").map((item, i) => (
-            <Items
-                key={i}
-                id={item.id}
-                image1={item.image1}
-                image2={item.image2}
-                image3={item.image3}
-                name={item.name}
-                old_price={item.old_price}
-                new_price={item.new_price}
-                offer={item.offer}
-                size_options={item.size_options}
-                product_count={item.product_count}
+      </div>
+      <div className="items-con">
+        {all_product.filter((item) => item.category === "Kids").map((item, i) => (
+          <Items
+            key={i}
+            id={item.id}
+            image1={item.image1}
+            image2={item.image2}
+            image3={item.image3}
+            name={item.name}
+            old_price={item.old_price}
+            new_price={item.new_price}
+            offer={item.offer}
+            size_options={item.size_options}
+            product_count={item.product_count}
 
-            />
+          />
         ))}
+      </div>
     </div>
-</div>
   );
 }
 export default Kids;
